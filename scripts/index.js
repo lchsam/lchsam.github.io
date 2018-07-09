5c60ca2e47a0541af141fccff01c6183c67a4319
lchsam <chunghinlee@umass.edu>
2018-07-09 12:06:25 -0400

    Now use onload

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,38 @@
-let nameAnimation = lottie.loadAnimation({
-    container: document.getElementById('name'), // the dom element that will contain the animation
-    name: 'nameAnimation',
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    path: 'data.json' // the path to the animation json
-});
-
-// Play the animation when the json file is completely loaded.
-nameAnimation.addEventListener('data_ready', () => lottie.play('nameAnimation'));
-
-// When animation is completed
-const whenNameCompleted = () => {
-    let buttons = document.getElementsByClassName("button");
-    [].forEach.call(buttons, (elem) => elem.classList.remove('inactiveLink'));
-    // buttons is not actually an array so it doesn't have forEach
-    let animationTimeline = anime.timeline();
-    animationTimeline.add({
-        targets: '#name',
-        translateY: [10, 0],
-        scale: '0.8',
-        easing: 'easeOutExpo',
-    }).add({
-        targets: ['.description', '.button'],
-        translateY: [50, 0],
-        opacity: 1,
-        easing: 'easeOutExpo',
-        offset: 100,
-        delay: (target, index) => index * 80,
+const init = () => {
+    let nameAnimation = lottie.loadAnimation({
+        container: document.getElementById('name'), // the dom element that will contain the animation
+        name: 'nameAnimation',
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        path: 'data.json' // the path to the animation json
     });
+    
+    // Play the animation when the json file is completely loaded.
+    nameAnimation.addEventListener('data_ready', () => lottie.play('nameAnimation'));
+    
+    // When animation is completed
+    const whenNameCompleted = () => {
+        let buttons = document.getElementsByClassName("button");
+        [].forEach.call(buttons, (elem) => elem.classList.remove('inactiveLink'));
+        // buttons is not actually an array so it doesn't have forEach
+        let animationTimeline = anime.timeline();
+        animationTimeline.add({
+            targets: '#name',
+            translateY: [10, 0],
+            scale: '0.8',
+            easing: 'easeOutExpo',
+        }).add({
+            targets: ['.description', '.button'],
+            translateY: [50, 0],
+            opacity: 1,
+            easing: 'easeOutExpo',
+            offset: 100,
+            delay: (target, index) => index * 80,
+        });
+    };
+    
+    nameAnimation.addEventListener('complete', whenNameCompleted);
 };
 
-nameAnimation.addEventListener('complete', whenNameCompleted);
+window.onload = init;
